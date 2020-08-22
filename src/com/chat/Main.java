6c2a8e250ca6ebaa1bbfb825ee1f71f04396d57e
choisungwook <kgg1959@naver.com>
2020-08-23 03:44:46 +0900

    Add 서버소켓
    연결요청 1개만 수락, 요청받으면 hello메세지 전달

--- src/com/chat/Main.java
+++ src/com/chat/Main.java
@@ -1,8 +1,37 @@
 package com.chat;
 
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int port = 10000;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while(true){
+                System.out.println("연결 대기중");
+                Socket socket = serverSocket.accept();
+
+                System.out.println("연결요청 수락" + socket.getInetAddress());
+
+                //데이터 전송
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(out);
+
+                dos.writeUTF("hello");
+                System.out.println("데이터 전송 끝");
+
+                dos.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
